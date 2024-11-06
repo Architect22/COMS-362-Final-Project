56befc6ba37172f6a4869039560457f5a81c0477
Ben Brown <bentombrown@gmail.com>
2024-11-06 12:42:01 -0600

    updated employee and storefront

--- COMS_309_Final_Project/src/core/Employee.java
+++ COMS_309_Final_Project/src/core/Employee.java
@@ -16,19 +16,62 @@ public class Employee {
 	}
 	
 	private void GenerateTaskList(String accessLevel) {
+		System.out.println("|----------------------------------------|");
+		System.out.println("|                  TASKS                 |");
 		if(accessLevel.equals("manager")) {
-			System.out.println("Tasks: Inventory");
+			System.out.println("| 1. Inventory                           |");
+			System.out.println("|----------------------------------------|");
+			System.out.print("Enter task to complete: ");
+			String task = in.nextLine().toLowerCase();
+			ChooseTask(task);
+			System.out.println();
+			System.out.println();
 		}
 		else if(accessLevel.equals("employee")) {
-			System.out.println("Tasks: Stocking, Cleaning");
+			System.out.println("| 1. Stocking                            |");
+			System.out.println("| 2. Cleaning                            |");
+			System.out.println("|----------------------------------------|");
+			System.out.print("Enter task to complete: ");
+			String task = in.nextLine().toLowerCase();
+			ChooseTask(task);
 		}
+	}
+	
+	private void ChooseTask(String task) {
+		if(task.equals("stocking")) {
+			StockShelves();
+		}
+		else if(task.equals("cleaning")) {
+			Clean();
+		}
+		else {
+			System.out.print("Nothing to do for this task.");
+		}
+		System.out.println();
+		System.out.println();
 	}
 	
 	public void StockShelves() {
-		
+		System.out.println(
+				"1. Walk through isles checking what products need stocking\r\n"
+				+ "2. Go to backroom\r\n"
+				+ "3. Find related stock\r\n"
+				+ "4. Bring stock to isle\r\n"
+				+ "5. Pull later expiration dates to front\r\n"
+				+ "6. Put back stock behind any existing items\r\n"
+				+ "7. Face up the product\r\n"
+				+ "8. Throw away trash\r\n"
+				+ "");
 	}
 	
 	public void Clean() {
-		
+		System.out.println(
+				"1. Go to an area to clean\r\n"
+				+ "2. Determine if the area needs cleaning\r\n"
+				+ "3. Throw away any trash in area\r\n"
+				+ "4. Sanitize the area\r\n"
+				+ "5. Wipe down area\r\n"
+				+ "6. Go to next area\r\n"
+				+ "");
 	}
 }
--- COMS_309_Final_Project/src/core/StoreFront.java
+++ COMS_309_Final_Project/src/core/StoreFront.java
@@ -6,20 +6,23 @@ public class StoreFront {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
-		System.out.println("|----------------------------------------|");
-		System.out.println("|            Grocery Store               |");
-		System.out.println("|                                        |");
-		System.out.println("|________________________________________|");
-		
-		System.out.println("Login: (Admin)(Employee)(Customer)");
-		System.out.print("Please enter your login type:");
-		String loginType = in.nextLine().toLowerCase();
-		
-		if(loginType.equals("employee")) {
-			Employee employee = new Employee();
+		while(true) {
+			System.out.println("|----------------------------------------|");
+			System.out.println("|            Grocery Store               |");
+			System.out.println("|                                        |");
+			System.out.println("|________________________________________|");
+			
+			System.out.println("Login: (Admin)(Employee)(Customer)(Exit)");
+			System.out.print("Please enter your login type:");
+			String loginType = in.nextLine().toLowerCase();
+			
+			if(loginType.equals("employee")) {
+				Employee employee = new Employee();
+			}
+			if(loginType.equals("exit")) {
+				break;
+			}
 		}
-		
 		in.close();
 	}
 
