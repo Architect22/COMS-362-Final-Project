8672ca4fa1f6b754efc3c5e2867e2b908a9c987b
Ben Brown <bentombrown@gmail.com>
2024-11-19 14:46:00 -0600

    created meat department tasks

--- COMS_309_Final_Project/src/core/Departments/FloralDepartment.java
+++ COMS_309_Final_Project/src/core/Departments/FloralDepartment.java
@@ -1,6 +1,5 @@
 package core.Departments;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
--- COMS_309_Final_Project/src/core/Departments/MeatDepartment.java
+++ COMS_309_Final_Project/src/core/Departments/MeatDepartment.java
@@ -1,5 +1,8 @@
 package core.Departments;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 import core.Utility;
 
 public class MeatDepartment extends Department {
@@ -21,9 +24,32 @@ public class MeatDepartment extends Department {
         Utility.createHeader(width, "Meat Department");
         System.out.println("| 1. Handle Customer                                           |");
         System.out.println("|--------------------------------------------------------------|");
+        handleCustomerOrder();
     }
 
-    private void handleCustomer() {
+    private void handleCustomerOrder() {
+        Scanner inString = new Scanner(System.in);
+        Scanner inFloat = new Scanner(System.in);
+        HashMap<String, Float> order = new HashMap<>();
+        boolean selecting = true;
+        while (selecting) {
+            System.out.print("Enter the meat for you want: ");
+            String meat = inString.nextLine();
 
+            System.out.print("Enter the amount (in lbs) for this meat: ");
+            float amount = inFloat.nextFloat();
+
+            System.out.print("Is that all: (y/n)");
+            String quit = inString.nextLine();
+
+            if (quit.equals("y")) {
+                selecting = false;
+            }
+            order.put(meat, amount);
+        }
+        Utility.displayLoadingAnimation(1, 500, "Getting your order ready...");
+        Utility.displayLoadingAnimation(1, 500, "Wrapping meat...");
+        Utility.displayLoadingAnimation(1, 500, "Tying up package...");
+        System.out.println("Your order of " + order.entrySet().toString() + " was successfully created!");
     }
 }
