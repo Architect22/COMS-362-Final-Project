a39b946af72180e2f8b08dd92195948e782a7508
Ben Brown <bentombrown@gmail.com>
2024-12-03 16:26:16 -0600

    project imports

--- COMS_309_Final_Project/src/core/Departments/Department.java
+++ COMS_309_Final_Project/src/core/Departments/Department.java
@@ -2,6 +2,8 @@ package core.Departments;
 
 import java.util.Scanner;
 
+import core.PharmacyDepartment;
+import core.SecurityDepartment;
 import core.Utility;
 
 public class Department {
@@ -34,42 +36,48 @@ public class Department {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
 
-        switch (input.toLowerCase())
-        {
-        	case "1": case "floral department":
-            FloralDepartment floral = FloralDepartment.getInstance();
-            floral.displayDepartmentTasks();
-        	break;
-        	
-        	case "2": case "meat department":
-            MeatDepartment meat = MeatDepartment.getInstance();
-            meat.displayDepartmentTasks();
-            break;
+        switch (input.toLowerCase()) {
+            case "1":
+            case "floral department":
+                FloralDepartment floral = FloralDepartment.getInstance();
+                floral.displayDepartmentTasks();
+                break;
 
-        	case "3": case "security department":
-            SecurityDepartment security = SecurityDepartment.getInstance();
-            security.displayDepartmentTasks();
-            break;
-            
-        	case "4": case "pharmacy department":
-            PharmacyDepartment pharmacy = PharmacyDepartment.getInstance();
-            pharmacy.displayDepartmentTasks();
-            break;
-            
-        	case "5": case "human resources department":
-        	HumanResourcesDepartment hr = HumanResourcesDepartment.getInstance();
-        	hr.displayDepartmentTasks();
-        	break;
+            case "2":
+            case "meat department":
+                MeatDepartment meat = MeatDepartment.getInstance();
+                meat.displayDepartmentTasks();
+                break;
 
-            case "6": case "online order department":
-            OnlineOrderDepartment onlineOrder = OnlineOrderDepartment.getInstance();
-            onlineOrder.displayDepartmentTasks();
-            break;
-            
-        	case "7": case "receipt verification department":
-        	ReceiptVerificationDepartment receiptVerification = ReceiptVerificationDepartment.getInstance();
-            receiptVerification.displayDepartmentTasks();
-        	break;
+            case "3":
+            case "security department":
+                SecurityDepartment security = SecurityDepartment.getInstance();
+                security.displayDepartmentTasks();
+                break;
+
+            case "4":
+            case "pharmacy department":
+                PharmacyDepartment pharmacy = PharmacyDepartment.getInstance();
+                pharmacy.displayDepartmentTasks();
+                break;
+
+            case "5":
+            case "human resources department":
+                HumanResourcesDepartment hr = HumanResourcesDepartment.getInstance();
+                hr.displayDepartmentTasks();
+                break;
+
+            case "6":
+            case "online order department":
+                OnlineOrderDepartment onlineOrder = OnlineOrderDepartment.getInstance();
+                onlineOrder.displayDepartmentTasks();
+                break;
+
+            case "7":
+            case "receipt verification department":
+                ReceiptVerificationDepartment receiptVerification = ReceiptVerificationDepartment.getInstance();
+                receiptVerification.displayDepartmentTasks();
+                break;
 
         }
     }
