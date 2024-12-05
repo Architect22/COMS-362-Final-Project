d3777e15715809196f7bf97c3c241b9ad6cd710e
Ben Brown <bentombrown@gmail.com>
2024-12-05 16:39:12 -0600

    Update BakeryDepartment.java

--- COMS_309_Final_Project/src/core/Departments/BakeryDepartment.java
+++ COMS_309_Final_Project/src/core/Departments/BakeryDepartment.java
@@ -1,6 +1,7 @@
 package core.Departments;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import core.Utility;
 import core.Tasks.BakeryTask;
@@ -9,9 +10,20 @@ import core.Tasks.TaskType;
 
 public class BakeryDepartment extends Department {
     public static BakeryDepartment instance;
+    public ArrayList<String> fullMenu = new ArrayList<>();
+    private ArrayList<String> dailyMenu = new ArrayList<>();
 
     private BakeryDepartment(String name) {
         super(name);
+        fullMenu.add("Muffin");
+        fullMenu.add("Cake");
+        fullMenu.add("Donut");
+        fullMenu.add("Brownie");
+        fullMenu.add("Bread");
+        fullMenu.add("Colache");
+        fullMenu.add("Cinnamon Roll");
+        fullMenu.add("Cookies");
+
     }
 
     public static BakeryDepartment getInstance() {
@@ -30,6 +42,23 @@ public class BakeryDepartment extends Department {
     }
 
     public void BakeItem() {
+        randomizeMenu();
+        for (String item : dailyMenu) {
+            Utility.displayLoadingAnimation(1, 500, "Baking " + item);
+        }
+        System.out.println("All items baked!");
+        System.out.println("The daily menu is: ");
+        for (String item : dailyMenu) {
+            System.out.println(item);
+        }
+    }
 
+    private void randomizeMenu() {
+        dailyMenu.clear();
+        Random rand = new Random();
+        int amount = rand.nextInt(fullMenu.size());
+        for (int i = 0; i < amount; ++i) {
+            dailyMenu.add(fullMenu.get(i));
+        }
     }
 }
